c3367808e51f4f8095c8074b53d001ccce264a3e
Kevin Blowers <kblowers@gmail.com>
2018-06-11 19:12:48 -0400

    Updated PU output

--- apftcalc.py
+++ apftcalc.py
@@ -7,8 +7,8 @@ situps = int(situps)
 female17_21PUScore = [29,30,32,34,36,37,39,41,43,44,46,48,50,51,53,55,57,58,60,62,63,65,67,69,70,72,74,76,77,79,81,83,84,86,88,90,91,93,95,97,98,100]
 female17_21PUReps = [i for i in range (1,51)]
 reps = female17_21PUReps.index(pushups)
-score = female17_21PUScore[reps]
-print (score)
+PUscore = female17_21PUScore[reps]
+print ("Pushup score {} ".format(PUscore))
 
 
 
